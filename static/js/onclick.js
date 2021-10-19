0167ff05780d45653d765753624c4980896a81eb
alexli1998 <alexli1998@live.com>
2021-10-19 06:49:06 +0000

    color

--- static/js/onclick.js
+++ static/js/onclick.js
@@ -25,14 +25,14 @@ function upload_img() {
 }
 
 $(function() {
-    $("#recommend").click(function (event) {
+    $("#generate").click(function (event) {
         $.ajax({
-                url: '/get_recommendation',
+                url: '/generate',
                 type: 'post',
                 data: {'img': img},
                 dataType: 'json',
                 success: function() {
-                    $.getJSON('/get_recommendation', {'img': img}, function(response) {
+                    $.getJSON('/generate', {'img': img}, function(response) {
                         document.getElementById('recimg').src = response.url
                     });
                 },
@@ -43,70 +43,12 @@ $(function() {
     });
 });
 
-$(".testImg").click(function(){
+$(".hairstyle").click(function(){
     $(this).addClass("sel");
-    $(this).siblings('.testImg').removeClass("sel");
+    $(this).siblings('.hairstyle').removeClass("sel");
 });
 
-
-
-//function upload_img()
-//{
-//    var SqlQuery = "select * from inventory";
-//    var rs = new ActiveXObject("ADODB.Recordset");
-//    rs.open(SqlQuery, connection);
-//
-//    var result="";
-//    while(!rs.eof)
-//        {
-//            result+=rs.Fields("product")+'\t'+rs.Fields("quantity")+'<br/>';
-//            rs.moveNext();
-//        }
-//    document.getElementById("INVENTORY").innerHTML = result;
-//}
-//
-//function getOrder()
-//{
-//    //初始化
-//    var orderString;
-//    var lines;
-//    var result="";
-//
-//    orderString = document.getElementById('ORDER').value;
-//    matchOrder(orderString,lines,result);
-//}
-//
-//function matchOrder(orderString,lines,result)
-//{
-//    lines=orderString.split('\n');
-//
-//    //查询                        
-//    var i;
-//    for (i = 0; i < lines.length; i++) 
-//        {
-//            var record=lines[i].split(" ");
-//            var SqlQuery = "select * from inventory where product='"+record[0]+"'";
-//            var rs = new ActiveXObject("ADODB.Recordset");
-//            rs.open(SqlQuery, connection);
-//            if(!rs.eof)
-//            {
-//                var quantity=rs.Fields("quantity");
-//
-//                var temp=parseInt(quantity)-parseInt(record[1]);
-//
-//                if(temp>=0)//判断库存能否满足订单
-//                {
-//                    result+=record[0]+'\t'+record[1]+"仓"+'<br/>';
-//                }
-//                else
-//                {
-//                    result+=record[0]+'\t'+record[1]+"买"+'<br/>';
-//                }
-//            }
-//            else
-//                result+=record[0]+'\t'+record[1]+'<br/>';
-//        }    
-//    document.getElementById("OUTPUT").innerHTML = result;
-//}
-
-
+$(".color").click(function(){
+    $(this).addClass("sel");
+    $(this).siblings('.color').removeClass("sel");
+});
